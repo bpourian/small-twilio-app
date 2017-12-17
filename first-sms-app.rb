93ed9b0fe6e6eb4aa28ba9b0c7e252e4bad8a153
Benjamin Pourian <b.pourian@hotmail.co.uk>
2017-12-17 08:32:39 +0000

    Updated require gem line

--- first-sms-app.rb
+++ first-sms-app.rb
@@ -1,4 +1,5 @@
-require "twilio-ruby" #Tell ruby that we require this gem
+require 'bundler/setup'
+#require "twilio-ruby" #Tell ruby that we require this gem
 #put your credentials here, get them at twilio.com
 account_sid = 'ACxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
 auth_token = 'yyyyyyyyyyyyyyyyyyyyyyyyyyyyyyy'
@@ -7,8 +8,8 @@ auth_token = 'yyyyyyyyyyyyyyyyyyyyyyyyyyyyyyy'
 client = Twilio::REST::Client.new account_sid, auth_token
 
 #send an sms
-client.account.sms.message.create(
-  :from => '+122002020202',
-  :to => '+928828282282',
-  :body =. 'I\'ve learnt about a new technology this morning'
+client.accounts.sms.message.create(
+  :from => '+786366335633',
+  :to => '+334438838383',
+  :body => 'I\'ve learnt about a new technology this morning'
 )
