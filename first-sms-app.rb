161f58a2623ee0644309e9cc0981ac4f5669a39f
Benjamin Pourian <b.pourian@hotmail.co.uk>
2017-12-17 09:12:33 +0000

    program finalised and tested

--- first-sms-app.rb
+++ first-sms-app.rb
@@ -1,6 +1,10 @@
+#There is video instructions from twilio which you can follow
+#The app can be run by typing in 'ruby first-sms-app.rb' or
+#'bundler exec ruby first-sms-app.rb'
+#vide link: https://www.twilio.com/docs/quickstart/ruby/sms#faq
 require 'bundler'
 Bundler.require()
-#require "twilio-ruby" #Tell ruby that we require this gem
+
 #put your credentials here, get them at twilio.com
 account_sid = "ACfghdhj67628237827hj2h9h98d338"
 auth_token = "ee782781u3hjk3ku83hd8839d89h9d8"
